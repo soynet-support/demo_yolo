1ab78889a6c65e419d140a01fc937c8626472193
soynet-support <74886743+soynet-support@users.noreply.github.com>
2020-11-23 19:00:01 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir mgmt/logs mgmt/engines mgmt/weights ./samples/3rdParty
-wget -o ./mgmt/weights/yolov4.weights https://soynet.io/demo/yolov4.weights
+cd mgmt/weights && wget https://soynet.io/demo/yolov4.weights && cd ../../
