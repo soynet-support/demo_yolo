359368b80e9b0b2f02c45da052c08dfc68a41310
soynet-support <74886743+soynet-support@users.noreply.github.com>
2020-11-27 18:10:54 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir mgmt/logs mgmt/engines mgmt/weights ./samples/3rdParty
+mkdir mgmt/logs mgmt/engines mgmt/weights
 cd mgmt/weights && wget https://soynet.io/demo/yolov4.weights && cd ../../
 #cd samples/include && wget https://soynet.io/demo/opencv.tgz && tar xzvf opencv.tgz && cd ../../
