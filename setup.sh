9f16d55e8aaa07992c462d4832f330e7131437f6
soynet-support <74886743+soynet-support@users.noreply.github.com>
2020-12-14 17:10:31 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd mgmt/weights && \
-  wget https://soynet.io/demo/yolov3-tiny.weights && \
-  wget https://soynet.io/demo/yolov3.weights && \
-  wget https://soynet.io/demo/yolov4.weights && \
-  cd ../../
