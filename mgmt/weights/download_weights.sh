82e85bdec23d931b06bdc6ac4281e17580de28eb
soynet-support <74886743+soynet-support@users.noreply.github.com>
2020-12-17 09:35:56 +0900

    Update download_weights.sh

--- mgmt/weights/download_weights.sh
+++ mgmt/weights/download_weights.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://soynet.io/demo/yolov3-tiny.weights && \
+wget https://soynet.io/demo/yolov3-tiny.weights
 wget https://soynet.io/demo/yolov3.weights
 wget https://soynet.io/demo/yolov4.weights
